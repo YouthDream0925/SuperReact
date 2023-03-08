923046299bc3c3cc9ce03d1e0798bcf4826b66b5
YouthDream0925 <youthdream0925@gmail.com>
2023-03-08 01:27:38 +0100

    Added contract code snap.

--- src/app/main/Address/components/Contract.js
+++ src/app/main/Address/components/Contract.js
@@ -11,86 +11,47 @@ import IconButton from '@material-ui/core/IconButton';
 import InboxIcon from '@material-ui/icons/Inbox';
 import DraftsIcon from '@material-ui/icons/Drafts';
 import { Link } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 export default function CustomPaginationActionsTable() {
+    const code = "<p>pragma solidity ^0.8.1;</p>" +    
+                    "contract SendMoneyExample {" + 
+                        "uint public balanceReceived;" +                         
+                        "function receiveMoney() public payable {" + 
+                            "balanceReceived += msg.value;" + 
+                        "}" +                     
+                        "function getBalance() public view returns(uint) {" + 
+                            "return address(this).balance;" + 
+                        "}" + 
+                        "function withdrawMoney() public {" + 
+                            "address payable to = payable(msg.sender);" + 
+                            "to.transfer(getBalance());"  +
+                        "}" + 
+                    "}";
     return (
         <>
-            <div>
-                <Button
-                    component={Link}
-                    to="/blocks"
-                    className="whitespace-nowrap mr-1"
-                    variant="contained"
-                    color="primary"
-                    >
-                    <span className="hidden sm:flex">Code</span>
-                </Button>
-                <Button
-                    component={Link}
-                    to="/blocks"
-                    className="whitespace-nowrap mr-1"
-                    variant="contained"
-                    color="primary"
-                    >
-                    <span className="hidden sm:flex">Read Contract</span>
-                </Button>
-                <Button
-                    component={Link}
-                    to="/blocks"
-                    className="whitespace-nowrap mr-1"
-                    variant="contained"
-                    color="primary"
-                    >
-                    <span className="hidden sm:flex">Write Contract</span>
-                </Button>
-            </div>
-            <div>
-                <List component="nav" aria-label="main mailbox folders">
-                    <ListItem>
-                        <div className='item-box'>
-                            <Icon className='mr-1'>help_outline</Icon>
-                            Contract Source Code Verified (Exact Match)
-                        </div>
-                    </ListItem>
-                    <ListItem>
-                        <div className='item-box'>
-                            Contract Name:
-                        </div>
-                        <div className='space'></div>
-                        <div className='item-box'>
-                            TetherToken
-                        </div>
-                        <div className='item-box'>
-                            Contract Name:
-                        </div>
-                        <div className='space'></div>
-                        <div className='item-box'>
-                            TetherToken
-                        </div>
-                    </ListItem>
-                    <ListItem>
-                        <div className='item-box'>
-                            <Icon className='mr-1'>help_outline</Icon>
-                            Timestamp:
-                        </div>
-                        <div className='space'></div>
-                        <div className='item-box'>
-                            <IconButton className="w-10 h-10"><Icon className='icon-font-size'>schedule</Icon></IconButton>
-                            1 min ago (Mar-05-2023 05:27:47 PM +UTC)
-                        </div>
-                    </ListItem>
-                    <ListItem>
-                        <div className='item-box'>
-                            <Icon className='mr-1'>help_outline</Icon>
-                            Proposed On:
-                        </div>
-                        <div className='space'></div>
-                        <div className='item-box'>
-                            Block proposed on slot 5934437, cpoch 185451
-                        </div>
-                    </ListItem>
-                </List>
-            </div>
+            <React.Fragment>
+                <CssBaseline />
+                <Container fixed>
+                    <Typography component="div" style={{ backgroundColor: '#1e2125', height: '100%', padding: '3rem' }}>
+                        <div>{"pragma solidity ^0.8.1;"}</div>
+                        <div>{"contract SendMoneyExample {"}</div>
+                        <div>{"uint public balanceReceived;"}</div>
+                        <div>{"function receiveMoney() public payable {"}</div>
+                        <div>{"balanceReceived += msg.value;"}</div>
+                        <div>{"}"}</div>
+                        <div>{"function getBalance() public view returns(uint) {"}</div>
+                        <div>{"return address(this).balance;"}</div>
+                        <div>{"}"}</div>
+                        <div>{"function withdrawMoney() public {"}</div>
+                        <div>{"address payable to = payable(msg.sender);"}</div>
+                        <div>{"to.transfer(getBalance());"}</div>
+                        <div>{"}"}</div>
+                    </Typography>
+                </Container>
+            </React.Fragment>
         </>
     );
 }
