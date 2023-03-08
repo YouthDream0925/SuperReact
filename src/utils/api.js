e5417a9fae0e534047577a39a42603da2069ef80
dreamPerson <dreamPerson0106@gmail.com>
2023-03-08 16:42:23 +0900

    update ports

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios
 const api = axios.create({
-  baseURL: 'http://localhost:5000/explorer',
+  baseURL: 'http://127.0.0.1:5000/explorer',
   headers: {
     'Content-Type': 'application/json; charset=utf-8',
   }
