a9e2bc4ebbd3a6abcf484acc7da7c134f1de8af1
dreamPerson <dreamPerson0106@gmail.com>
2023-03-08 16:36:58 +0900

    asdf

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios
 const api = axios.create({
-  baseURL: 'http://52.77.226.235:5000/explorer',
+  baseURL: 'http://localhost:5000/explorer',
   headers: {
     'Content-Type': 'application/json; charset=utf-8',
   }
