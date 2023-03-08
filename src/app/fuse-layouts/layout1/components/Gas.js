57845d025a3372274c15d55f35667c5d82e1d4bd
dreamPerson <dreamPerson0106@gmail.com>
2023-03-08 09:43:19 +0900

    update gas fee part

--- src/app/fuse-layouts/layout1/components/Gas.js
+++ src/app/fuse-layouts/layout1/components/Gas.js
@@ -1,9 +1,19 @@
-import { memo } from 'react';
+import { memo, useState, useEffect } from 'react';
 import Icon from '@material-ui/core/Icon';
 import IconButton from '@material-ui/core/IconButton';
 import { red } from '@material-ui/core/colors';
+import api from "../../../../utils/api.js";
+import {ethers} from "ethers";
 
 function Gas() {
+  const [gasPrice, setGasPrice] = useState(0);
+
+  useEffect(async () => {
+    const res = await api.get(`/getGasPrice`);
+    console.log("===================", res)
+    setGasPrice(ethers.BigNumber.from(res.gasPrice.hex));
+  }, [])
+
   return (
     <>
         <div className="gas">
@@ -11,7 +21,7 @@ function Gas() {
                 <Icon>local_gas_station</Icon>
             </IconButton>
             <div>
-                Gas: <strong style={{color: '#5395c9'}}>49 Gwei</strong>
+                Gas: <strong style={{color: '#5395c9'}}>{gasPrice} wei</strong>
             </div>
         </div>
     </>
---
+++ src/utils/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+// Create an instance of axios
+const api = axios.create({
+  baseURL: 'http://52.77.226.235:3000/explorer',
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8',
+  }
+});
+/*
+  NOTE: intercept any error responses from the api
+ and check if the token is no longer valid.
+ ie. Token has expired or user is no longer
+ authenticated.
+ logout the user if the token has expired
+*/
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default api;
