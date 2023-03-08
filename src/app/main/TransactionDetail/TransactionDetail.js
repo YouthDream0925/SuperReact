26100d9b9cd6101acf7f1069df552dea30bee8e6
dreamPerson <dreamPerson0106@gmail.com>
2023-03-08 14:30:35 +0900

    update

--- src/app/main/TransactionDetail/TransactionDetail.js
+++ src/app/main/TransactionDetail/TransactionDetail.js
@@ -10,6 +10,7 @@ import IconButton from '@material-ui/core/IconButton';
 import InboxIcon from '@material-ui/icons/Inbox';
 import DraftsIcon from '@material-ui/icons/Drafts';
 import './TransactionDetail.css';
+import api from "../../../utils/api.js";
 
 const useStyles = makeStyles((theme) => ({
   root: {
